6fdd463d48da9edafcd1255936da81b84247bbbd
huangyiduo <yiduo_huang@berkeley.edu>
2024-04-11 21:53:53 -0500

    add socp formulation

--- warehouse.py
+++ warehouse.py
@@ -1,9 +1,9 @@
 from workstations import Workstation_I1, Workstation_I2, Workstation_I3
-from util import EPS, plot_colored_grid
+from util import EPS, plot_colored_grid, workstation_travel_time
 from simulator import Simulator
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import colors
+import gurobipy as gp
+from gurobipy import GRB
 
 class Warehouse:
     def __init__(self, width, height, demand_density, min_distance, X, E_S1, Var_S1, E_S2, Var_S2, special_pod_size, has_XYZ = False, Y=None, Z=None):
@@ -130,7 +130,6 @@ class Warehouse:
             system_map[int(ws.x), int(ws.y)] = type_dict[ws.type]
         plot_colored_grid(system_map, colors=['white','green','blue','red'],bounds=[-0.5,0.5,1.5,2.5,3.5])
 
-
     def load_YZ(self, Y: dict, Z: dict):
         # Y[j,i] is the flow from cell j to i
         # Z[k,i] is the flow of special pod from k in I1 to i in I2
@@ -161,9 +160,88 @@ class Warehouse:
                 rho = Q_I2[i] * self.workstation_dict[i].E_service_time
                 assert rho < 1. - EPS
 
+    def solve_socp(self, alpha_list):
+        set_I = [ws.id for ws in self.workstation_dict.values()]
+        set_I1 = [ws.id for ws in self.workstation_dict.values() if ws.type=='I1']
+        set_I2 = [ws.id for ws in self.workstation_dict.values() if ws.type=='I2']
+        set_I3 = [ws.id for ws in self.workstation_dict.values() if ws.type=='I3']
+        set_I13 = set_I1 + set_I3
+        set_J = list(self.J_xy_dict.keys())
+        m = gp.Model("warehouse_workstation")
+        # add vars
+        X = m.addVars(set_I, lb=0, ub=1, vtype=GRB.BINARY, name='X')
+        max_demand_one_pod = np.max(self.demand_map)
+        Y = m.addVars(set_J, set_I, lb=0., ub=max_demand_one_pod, vtype=GRB.CONTINUOUS, name='Y')
+        Z = m.addVars(set_I1, set_I2, lb=0., ub=1./self.E_S1, vtype=GRB.CONTINUOUS, name='Z')
+        Q = m.addVars(set_I, lb=0., ub=max(1./self.E_S1, 1/self.E_S2), vtype=GRB.CONTINUOUS, name='Z')
+        V = m.addVars(set_I1, set_I2, lb=0, ub=1, vtype=GRB.BINARY, name='V')
+        G = m.addVars(set_I, lb=0., vtype=GRB.CONTINUOUS, name='G')
+        H = m.addVars(set_I, lb=0., vtype=GRB.CONTINUOUS, name='H')
+        # set obj function
+        # get parameters
+        big_M = 1./self.E_S2
+        demand_j = [self.demand_map[int(self.J_xy_dict[j][0])][int(self.J_xy_dict[j][1])] for j in set_J]
+        distance_ji = dict()
+        distance_ki = dict()
+        mu = [1./self.workstation_dict[i].E_service_time for i in set_I]
+        CV_S1_2 = self.Var_S1/self.E_S1/self.E_S1
+        CV_S2_2 = self.Var_S2/self.E_S2/self.E_S2
+        for j in set_J:
+            for i in set_I:
+                distance_ji[j,i] = abs(self.workstation_dict[i].x-self.J_xy_dict[j][0]) + \
+                                   abs(self.workstation_dict[i].y-self.J_xy_dict[j][1])
+        for k in set_I1:
+            for i in set_I2:
+                distance_ki[k,i] = workstation_travel_time(self.workstation_dict[k], self.workstation_dict[i])
+        cost_I13_sum = gp.quicksum([
+            alpha_list[i]*X[i] + Q[i]/mu[i]+ (1.+CV_S1_2)/2./mu[i]*G[i] +
+            gp.quicksum([
+                distance_ji[j,i]*Y[j,i]
+                for j in set_J
+            ])
+            for i in set_I13
+        ])
+        cost_I2_sum = gp.quicksum([
+            alpha_list[i] * X[i] + Q[i] / mu[i] + CV_S2_2 / 2. / mu[i] * G[i] +
+            gp.quicksum([
+                distance_ki[k, i] * Z[k, i]
+                for k in set_I1
+            ])
+            for i in set_I2
+        ])
+        m.setObjective(cost_I13_sum+cost_I2_sum, sense=GRB.MINIMIZE)
+        # constraints
+        m.addConstrs((Q[i] == gp.quicksum([Y[j,i] for j in set_J]) for i in set_I13), name='5_1') # 5.1
+        m.addConstrs((Q[i] == gp.quicksum([Z[k,i] for k in set_I1]) for i in set_I2), name='5_2') # 5.2
+        m.addConstrs((gp.quicksum([Y[j,i] for i in set_I13]) == demand_j[j] for j in set_J),name='5_10') #5.10
+        m.addConstrs((Q[i] <= X[i]*mu[i] for i in set_I), name='5_11') # 5.11
+        m.addConstrs((gp.quicksum([Z[k,i] for i in set_I2])==Q[k]/self.special_pod_size for k in set_I1), name='5_12') # 5.12
+        m.addConstrs((Z[k,i]<=big_M*V[k,i] for k in set_I1 for i in set_I2), name='5.13') #5.13
+        m.addConstrs((gp.quicksum([V[k,i] for i in set_I2])==1 for k in set_I1), name='5_14') # 5.14
+        m.addConstrs((Q[i]*Q[i] <= G[i]*H[i] for i in set_I),name='5_23') #5.23
+        m.addConstrs((H[i] == mu[i]*X[i]-Q[i] for i in set_I), name='5_24') #5.24
+        # make sure if one I1 open, then there must be at least one I2 open
+        m.addConstr(gp.quicksum([X[i] for i in set_I1])>=1, name='at_least_one_I1')
+        m.addConstr(gp.quicksum([X[i] for i in set_I2])>=1, name='at_least_one_I2')
+
+        # solve the problem
+        m.optimize()
+        print('Total cost:', m.ObjVal)
+
+        open_cost = 0.
+        print("SOLUTION:")
+        for i in set_I:
+            if X[i].X > 0.99:
+                print(f"workstation {i} open")
+                print('type:', self.workstation_dict[i].type)
+                open_cost += alpha_list[i]
+
+        print('cost without open cost:', m.ObjVal - open_cost)
+
+
 if __name__ =='__main__':
-    width=20
-    height=20
+    width=16
+    height=16
     min_distance = 4
     demand_density=0.05/min_distance/min_distance
 
@@ -176,10 +254,12 @@ if __name__ =='__main__':
     Var_S2=16.
     special_pod_size=100
     warehouse = Warehouse(width, height, demand_density, min_distance, X, E_S1, Var_S1, E_S2, Var_S2, special_pod_size)
-    #warehouse.plot_system()
-    simulation_steps = 100000.
-    simulator = Simulator(warehouse.workstation_dict, warehouse.arrival_rate_dict, simulation_steps)
-    simulator.run_simulation()
-    for ws in warehouse.workstation_dict.values():
-        if ws.type =='I2':
-            ws.flow_stats(total_time=simulation_steps)
+    set_I = [ws.id for ws in warehouse.workstation_dict.values()]
+    #warehouse.solve_socp(alpha_list=[10. for i in set_I])
+    warehouse.plot_system()
+    # simulation_steps = 100000.
+    # simulator = Simulator(warehouse.workstation_dict, warehouse.arrival_rate_dict, simulation_steps)
+    # simulator.run_simulation()
+    # for ws in warehouse.workstation_dict.values():
+    #     if ws.type =='I2':
+    #         ws.flow_stats(total_time=simulation_steps)
